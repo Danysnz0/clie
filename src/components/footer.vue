ac67e725bc7371b2db6508836e031ea5fddcfe8b
Danysnz0 <dysanchez58@misena.edu.co>
2021-11-03 22:13:14 -0500

    Vue repaso

---
+++ src/components/footer.vue
@@ -0,0 +1,28 @@
+
+    <template>
+      <div>
+        <v-container class="block latestPostBlock">
+          Footer
+        </v-container>
+    </div>
+    </template>
+
+    <script>
+      export default {
+        name = "Footer",
+
+        data(){
+          return{
+            items:[
+            ]
+          };
+        }
+      };
+    </script>
+    
+    
+
+
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'inicio',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Inicio.vue')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/proyectos',
+    name: 'Proyectos',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Proyectos.vue')
+  },
+  {
+    path: '/contacto',
+    name: 'Contacto',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contacto.vue')
+  },
+
+  {
+    path: '/cartelera',
+    name: 'Cartelera',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Cartelera.vue')
+  },
+  {
+    path: '/senarautas',
+    name: 'Senarautas',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Senarautas.vue')
+  },
+  {
+    path: '/registro',
+    name: 'Registro',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Registro.vue')
   }
+
+
 ]
 
 const router = new VueRouter({
